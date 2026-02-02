4faa781c2ccabdda2acb71cd71b0b4b63e18b217
Ignacio <185011694+nimbusphagia@users.noreply.github.com>
2026-02-02 18:07:34 -0500

    add main forms

--- public/scripts/mainForms.js
+++ public/scripts/mainForms.js
@@ -1,6 +1,8 @@
 const main = document.querySelector('.wrapper');
 const curtain = document.getElementById('curtain');
 const createFolderForm = document.querySelector('.createFolder');
+const editFolderForm = document.querySelector('.editFolder');
+const deleteFolderForm = document.querySelector('.deleteFolder');
 const createFileForm = document.querySelector('.createFile');
 
 function toggleCurtain() {
@@ -20,13 +22,26 @@ function hideAll() {
   toggleCurtain();
 }
 main.addEventListener('click', (e) => {
-  if (!e.target.closest('.btn')) return;
+  if (!e.target.closest('.btn') && !e.target.closest('.fakeBtn')) return;
   const btn = e.target.closest('.btn');
-  // New folder
-  if (btn.classList.contains('newBtn')) {
-    toggleForm(createFolderForm);
-  } else if (btn.classList.contains('treeBtn')) {
-    toggleForm(createFileForm);
+  const fakeBtn = e.target.closest('.fakeBtn');
+  if (btn) {
+    if (btn.classList.contains('newBtn')) {
+      // New folder
+      toggleForm(createFolderForm);
+    } else if (btn.classList.contains('treeBtn')) {
+      // New file
+      toggleForm(createFileForm);
+    }
+  } else if (fakeBtn) {
+    if (fakeBtn.classList.contains('editBtn')) {
+      // Delete folder
+      toggleForm(editFolderForm);
+    } else if (fakeBtn.classList.contains('deleteBtn')) {
+      // Delete folder
+      toggleForm(deleteFolderForm);
+    }
+
   }
 
 });
